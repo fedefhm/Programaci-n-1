6ac8ede4e45b64961a472e85b3f7d645c497664d
Federico Macri <federicohmacri@gmail.com>
2018-03-22 21:04:36 -0300

    cambio tal cosa por otro

--- Clase1_2/main.c
+++ Clase1_2/main.c
@@ -10,16 +10,17 @@ int main()
     int numeroUsuario;
     int acumulador = 0;
     float resultado;
-    int maximo;
+    int maximo; //podría acotar con un INT_MIN si arriba incluí el "define" con la biblioteca "limits.h" (ver apunte)
     int minimo;
     for(i=0 ; i < CANTIDAD_NUMEROS ; i++)
     {
         printf("Dame el Numero %d\n",i+1);
         scanf("%d",&numeroUsuario);
         acumulador = acumulador + numeroUsuario;
-        if(i==0 || numeroUsuario>maximo)
+        if(i==0 || numeroUsuario>maximo) //if( flag==0 || numeroUsuario>maximo) debo declarar int flag=0
         {
             maximo=numeroUsuario;
+            //flag=1 para que salte el flag. Con 1 solo = xq no comparo, quiero el valor.
         }
                 if(i==0 || numeroUsuario<minimo)
         {
